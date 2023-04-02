6ef9e1dc9f0d71b1cf80abc1bcff92ab739a5dff
raejun92 <cvcv1324@naver.com>
2023-04-02 11:50:49 +0900

    getStaticPorps test

---
+++ pages/test.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function PostPage({ post }) {
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+
+  const paths = posts.map((post) => ({
+    params: { id: post.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+  const post = await res.json();
+
+  return {
+    props: {
+      post,
+    },
+  };
+}
